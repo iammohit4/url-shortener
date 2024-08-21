8351a47c54998f8d653536848c5756a3c3cd68a7
Mohit <thisisblinz@gmail.com>
2024-08-22 01:23:37 +0530

    code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,8 +19,8 @@ const About = () => {
         <div>
             <h1 className="text-accent text-2xl font-bold">About</h1>
             <p>This is a URL Shortener made using Typescript, Next.js 14, DaisyUI, Tailwind CSS and an open source url shortener api.</p>
-            <Link href="https://github.com/iammohit4/url-shortener" className="btn btn-outline m-1">Source Code</Link>
-            <Link href="https://linkedin.com/in/mohit-sundrani-11470a309" className="btn btn-primary m-1"><FaLinkedin /></Link>
+            <Link href="https://github.com/iammohit4/url-shortener" className="btn btn-outline m-1" target="_blank">Source Code</Link>
+            <Link href="https://linkedin.com/in/mohit-sundrani-11470a309" className="btn btn-primary m-1" target="_blank"><FaLinkedin /></Link>
         </div>
     )
 }
